d1384af3deab52f16da57bdb0f6b1c0840cae85a
Thomas Palma <thomaspalma777@gmail.com>
2021-09-15 21:59:04 -0300

    creating a class with three attributes to better represent the triangle

--- src/oop/app/App.java
+++ src/oop/app/App.java
@@ -2,42 +2,43 @@ package oop.app;
 
 import java.util.Locale;
 import java.util.Scanner;
+import oop.app.Triangle;
 
 public class App {
 
 	public static void main(String[] args) {
-			Scanner sc = new Scanner(System.in);
-			Locale.setDefault(Locale.US);
-			
-			double xA,xB,xC,yA,yB,yC;
-			
-			System.out.println("Enter the measures of triangle X: ");
-			xA = sc.nextDouble();
-			xB = sc.nextDouble();
-			xC = sc.nextDouble();
-			System.out.println("Enter the measures of triangle Y: ");
-			yA = sc.nextDouble();
-			yB = sc.nextDouble();
-			yC = sc.nextDouble();
-			
-			double p = (xA + xB +xC) /2.0;
-			double areaX = Math.sqrt(p*(p - xA) *(p - xB) * (p - xC));
-			
-			p = (yA + yB +yC) /2.0;
-			double areaY = Math.sqrt(p*(p - yA) *(p - yB) * (p - yC));
-			
-			
-			System.out.printf("Triangle X area: %.4f%n", areaX);
-			System.out.printf("Triangle y area: %.4f%n", areaY);
-			
-			if (areaX > areaY) {
-				System.out.println("Larger area: X");
-			}
-			else {
-				System.out.println("Larger area: Y");
-			}
-			
-			sc.close();
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+
+		Triangle x, y;
+		x = new Triangle();
+		y = new Triangle();
+		
+		System.out.println("Enter the measures of triangle X: ");
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
+		System.out.println("Enter the measures of triangle Y: ");
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
+
+		double p = (x.a + x.b + x.c) / 2.0;
+		double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
+
+		p = (y.a + y.b + y.c) / 2.0;
+		double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+
+		System.out.printf("Triangle X area: %.4f%n", areaX);
+		System.out.printf("Triangle y area: %.4f%n", areaY);
+
+		if (areaX > areaY) {
+			System.out.println("Larger area: X");
+		} else {
+			System.out.println("Larger area: Y");
+		}
+
+		sc.close();
 	}
 
 }
---
+++ src/oop/app/Triangle.java
@@ -0,0 +1,9 @@
+package oop.app;
+
+public class Triangle {
+		
+		double a;
+		double b;
+		double c;
+
+	}
