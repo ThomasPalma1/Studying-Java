675947741b48643180d9c42cd0b45d9904340b0c
Thomas Palma <thomaspalma777@gmail.com>
2021-09-20 20:39:04 -0300

    creating a method for reusing and delegating benefits

--- src/oop/app/App.java
+++ src/oop/app/App.java
@@ -23,11 +23,9 @@ public class App {
 		y.b = sc.nextDouble();
 		y.c = sc.nextDouble();
 
-		double p = (x.a + x.b + x.c) / 2.0;
-		double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
+		double areaX = x.area();
 
-		p = (y.a + y.b + y.c) / 2.0;
-		double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
+		double areaY = y.area();
 
 		System.out.printf("Triangle X area: %.4f%n", areaX);
 		System.out.printf("Triangle y area: %.4f%n", areaY);
--- src/oop/app/Triangle.java
+++ src/oop/app/Triangle.java
@@ -6,4 +6,9 @@ public class Triangle {
 		double b;
 		double c;
 
+		public double area() {
+			double p = (a + b + c) /2.0;
+			return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+
+		}
 	}
