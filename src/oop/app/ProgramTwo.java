fc2f58582773ffaa70e401af183ae11d0cb58051
Thomas Palma <thomaspalma777@gmail.com>
2021-10-25 21:22:00 -0300

    second version of the exercise resolution

--- src/oop/app/ProgramTwo.java
+++ src/oop/app/ProgramTwo.java
@@ -2,31 +2,29 @@ package oop.app;
 
 import java.util.Scanner;
 
+import util.Calculator;
+
 public class ProgramTwo {
 
-	public static final double PI = 3.14159;
+
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
+		Calculator calc = new Calculator();
+		
 		System.out.print("Enter radius: ");
 		double radius = sc.nextDouble();
-		double c = circumference(radius);
-		double v = volume(radius);
+		double c = calc.circumference(radius);
+		double v = calc.volume(radius);
 
 		System.out.printf("Circumference: %.2f%n", c);
 		System.out.printf("Volume: %.2f%n", v);
-		System.out.printf("PI: %.2f%n", PI);
+		System.out.printf("PI: %.2f%n", calc.PI);
 
 		sc.close();
 
 	}
 
-	public static double circumference(double radius) {
-		return 2.0 * PI * radius;
-	}
-
-	public static double volume(double radius) {
-		return 4.0 * PI * radius * radius * radius /3.0;
-	}
+	
 }
