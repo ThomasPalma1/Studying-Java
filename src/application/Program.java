0d275f4ccaea5dc72f85312f1d9bc0f3e74d1582
Thomas Palma <66331874+ThomasPalma1@users.noreply.github.com>
2021-11-08 15:38:58 -0300

    starting the solution of another exercise

--- src/application/Program.java
+++ src/application/Program.java
@@ -22,17 +22,24 @@ public class Program {
 		System.out.println("Update name: " + product.getName());
 		product.setPrice(1200.00);
 		System.out.println("Update price: " + product.getPrice());
+
 		System.out.println();
+
 		System.out.println("Product data: " + product);
+
 		System.out.println();
+
 		System.out.print("Enter the number of products to be added in stock: ");
 		int quantity = sc.nextInt();
 
 		product.addProducts(quantity);
 
 		System.out.println();
+
 		System.out.println("Updated data: " + product);
+
 		System.out.println();
+
 		System.out.print("Enter the number of products to be removed from stock: ");
 		quantity = sc.nextInt();
 
--- src/entities/Product.java
+++ src/entities/Product.java
@@ -19,19 +19,19 @@ public class Product {
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public double getPrice() {
 		return price;
 	}
-	
+
 	public void setPrice(double price) {
 		this.price = price;
 	}
-	
+
 	public int getQuantity() {
 		return quantity;
 	}
