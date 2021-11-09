17c0c446fe89d30a0f02834943737099f7134f85
Thomas Palma <66331874+ThomasPalma1@users.noreply.github.com>
2021-11-09 15:42:54 -0300

    simple thread example for operating systems class

---
+++ src/thread/so/Main.java
@@ -0,0 +1,9 @@
+package thread.so;
+
+public class Main {
+
+	public static void main(String[] args) {
+		MinhaThread minhaThread = new MinhaThread();
+		minhaThread.start();
+	}
+}
---
+++ src/thread/so/MinhaThread.java
@@ -0,0 +1,8 @@
+package thread.so;
+
+public class MinhaThread extends Thread {
+
+	public void run() {
+		System.out.println("Executando Thread!");
+	}
+}
