b3624d4cc9f22ab29dcf93ffade2d16def264671
Thomas Palma <66331874+ThomasPalma1@users.noreply.github.com>
2021-11-10 18:52:08 -0300

    threads - test

--- src/thread/so/Main.java
+++ src/thread/so/Main.java
@@ -2,8 +2,23 @@ package thread.so;
 
 public class Main {
 
-	public static void main(String[] args) {
-		MinhaThread minhaThread = new MinhaThread();
-		minhaThread.start();
+	public static void main(String[] args) throws InterruptedException {
+		MinhaThread thread1 = new MinhaThread("1");
+		thread1.start();
+		thread1.waitNotification();
+		
+
+		MinhaThread thread2 = new MinhaThread("2");
+		thread2.start();
+		thread2.waitTimeIsOver(5000);
+
+		System.out.println("Finalizando a Thread2");
+		thread2.interrupt();
+
+		System.out.println("Tentando acessar a Thread2, já encerrada.");
+		thread2.start();
+		
+		System.out.println("Notificando a Thread1");
+		thread2.notifySomething();
 	}
 }
--- src/thread/so/MinhaThread.java
+++ src/thread/so/MinhaThread.java
@@ -1,8 +1,31 @@
 package thread.so;
 
 public class MinhaThread extends Thread {
+	
+	public String nome;
+	
+	MinhaThread (String nome) {
+		this.nome = nome;
+	}
 
 	public void run() {
-		System.out.println("Executando Thread!");
+		System.out.println("Executando Thread " + nome + "!");
+		
+		for (int i = 0; i < 5; i++) {
+			System.err.println("#" + nome + ": " + i);
+			
+		}
+	}
+	
+	public synchronized void waitNotification () throws InterruptedException {
+		this.wait();
+	}
+	
+	public synchronized void waitTimeIsOver (int time) throws InterruptedException {
+		this.wait(time);
+	}
+	
+	public synchronized void notifySomething () {
+		this.notifyAll();
 	}
 }
