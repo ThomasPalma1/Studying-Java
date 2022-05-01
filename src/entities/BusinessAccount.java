de2faddffead705e3840ee8667944fa4339d908f
Thomas Palma <66331874+ThomasPalma1@users.noreply.github.com>
2022-05-01 20:26:22 -0300

    working with inheritance and polymorphism
    - simple source code that will be working in the future

---
+++ src/entities/BusinessAccount.java
@@ -0,0 +1,27 @@
+package entities;
+
+public class BusinessAccount extends BankAccount {
+    private Double loanLimit;
+
+    public BusinessAccount() {
+    }
+
+    public BusinessAccount(Integer number, String holder, Double balance, Double loanLimit) {
+        super(number, holder, balance);
+        this.loanLimit = loanLimit;
+    }
+
+    public Double getLoanLimit() {
+        return loanLimit;
+    }
+
+    public void setLoanLimit(Double loanLimit) {
+        this.loanLimit = loanLimit;
+    }
+
+    public void loan(double amount) {
+        if (amount <= loanLimit) {
+            deposit(amount);
+        }
+    }
+}
